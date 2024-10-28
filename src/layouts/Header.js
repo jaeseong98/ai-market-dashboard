85ce46aa4419e02bc533b370995db6477ba2ad3e
Park jaeseong <jaeseong2418@gmail.com>
2024-10-28 09:49:39 +0900

    Make header title clickable and redirect to home

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,10 +4,10 @@ import logo from '../assets/images/fnguide-logo.png'
 const Header = () => (
     <header className="bg-gray-900 text-white p-4 shadow-lg border-b border-gray-700">
         <div className="container mx-auto flex items-center">
-            <div className="flex items-center flex-shrink-0">
+            <Link to="/" className="flex items-center flex-shrink-0 hover:opacity-80">
                 <img src={logo} alt="FnGuide 로고" className="h-12 w-auto mr-4" />
                 <h1 className="text-2xl font-bold">미국 경제 대시보드</h1>
-            </div>
+            </Link>
             <nav className="flex space-x-6 ml-auto">
                 <Link to="/markets" className="text-gray-300 hover:text-white">Markets</Link>
                 <Link to="/economics" className="text-gray-300 hover:text-white">Economics</Link>
